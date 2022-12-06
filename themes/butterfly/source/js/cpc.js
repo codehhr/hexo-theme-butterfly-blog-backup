31d718e94c292e67e6aa97e0213bb200b6f424b5
codehhr <1871973389@qq.com>
2022-12-06 13:38:17 +0800

    ad

--- themes/butterfly/source/js/cpc.js
+++ themes/butterfly/source/js/cpc.js
@@ -1,11 +1,3 @@
-window.onload = function () {
-  // 4h44 cpc
-  document
-    .querySelector("[src='https://pic.wk4y.cn/pp/sm/05.jpg']")
-    .parentElement.click();
+window.onload = function () {};
 
-  // 3324 cpc
-  document
-    .querySelector("[src='https://pic.wk4y.cn/pp/sm/06.jpg']")
-    .parentElement.click();
-};
+// $(document).ready(function () {});
